a98791c98ea05bca1e05b32610960c883455920f
umihico <umihico@users.noreply.github.com>
2019-02-03 19:25:00 +0900

    add colmun title

--- laravel/bookapp/database/migrations/2019_02_03_101808_create_books_table.php
+++ laravel/bookapp/database/migrations/2019_02_03_101808_create_books_table.php
@@ -15,7 +15,7 @@ class CreateBooksTable extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->increments('id');
-            $table->increments('title');
+            $table->string('title');
             $table->timestamps();
         });
     }

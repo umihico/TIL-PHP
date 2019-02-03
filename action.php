b07143f26d30f7a775bbfe7cf0ba022244fc55f4
umihico <umihico@users.noreply.github.com>
2019-02-03 16:52:57 +0900

    learn action

---
+++ action.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<head>
+  <title>form action</title>
+</head>
+
+<body>
+ <h1>name and month</h1>
+ hello <?php echo $_POST['name'] ?>.
+ <br>
+ <?php echo $_POST['month'] ?> is your month.
+</body>
---
+++ form.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<head>
+  <title>form testing</title>
+</head>
+
+<body>
+  <form action="action.php" method="post">
+    name:<input type="text" name="name" value="">
+    month:<input type="text" name="month" value="">
+    <input type="submit" value='sbmt'>
+  </form>
+</body>

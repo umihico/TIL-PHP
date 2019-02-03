54627005d213f69413a884de51d9f8ca35ef88ed
umihico <umihico@users.noreply.github.com>
2019-02-04 00:03:47 +0900

    modified import of L5scaffold

--- laravel/todo/config/app.php
+++ laravel/todo/config/app.php
@@ -174,6 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Laralib\L5scaffold\GeneratorsServiceProvider::class,
 
     ],
 

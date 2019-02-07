8a2d9ee9993d9244757cf64aedd32d754ebeb00c
umihico <umihico@users.noreply.github.com>
2019-02-07 12:59:52 +0900

    import data from json to sqlite

--- laravel/tpw/database/migrations/2019_02_06_122457_create_portfolio_list_table.php
+++ laravel/tpw/database/migrations/2019_02_06_122457_create_portfolio_list_table.php
@@ -18,7 +18,7 @@ class CreatePortfolioListTable extends Migration
           $table->timestamps();
           $table->integer('forks');
           $table->string('full_name',100);
-          $table->json('geotags');
+          $table->text('geotags');
           $table->string('gif_path',100);
           $table->boolean('gif_success');
           $table->string('homepage',100);
@@ -26,7 +26,7 @@ class CreatePortfolioListTable extends Migration
           $table->string('html_url',100);
           $table->string('pushed_at',100);
           $table->string('reponame',100);
-          $table->json('skills');
+          $table->text('skills');
           $table->integer('stargazers_count');
           $table->string('username',100);
         });

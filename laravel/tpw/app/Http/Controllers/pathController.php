d7bd59a6658da473ed9aa7fb1c9498f3244e1324
umihico <umihico@users.noreply.github.com>
2019-02-07 15:03:47 +0900

    suceed to split funcs according to path

--- laravel/tpw/app/Http/Controllers/pathController.php
+++ laravel/tpw/app/Http/Controllers/pathController.php
@@ -4,13 +4,40 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Portfolio;
+
+
+
 class pathController extends Controller
 {
-  public function index($path)
+  public function pick_12portfolios($sortkey, $page_num)
   {
-    // $pfs = Portfolio::all();
     $pfs = Portfolio::where('username', 'umihico')->get();
-    // return view('select')->with('members',$pfs);
-    return view("path",['hoge' => $path, 'portfolios' => $pfs]);
+    return $pfs;
+  }
+  public function most_stars($path)
+  {
+    $display_portfolios=$this->pick_12portfolios('most_stars', $path);
+    return view("path",['hoge' => $path, 'portfolios' => $display_portfolios]);
+  }
+  public function most_forks($path)
+  {
+
+    $display_portfolios=$this->pick_12portfolios('most_forks', $path);
+    return view("path",['hoge' => $path, 'portfolios' => $display_portfolios]);
+  }
+  public function recently_update($path)
+  {
+    $display_portfolios=$this->pick_12portfolios('recently_update', $path);
+    return view("path",['hoge' => $path, 'portfolios' => $display_portfolios]);
+  }
+  public function all_locations()
+  {
+    $pfs = Portfolio::all();
+    return view("path",['hoge' => 'dummy', 'portfolios' => $pfs]);
+  }
+  public function all_users()
+  {
+    $pfs = Portfolio::all();
+    return view("path",['hoge' => 'dummy', 'portfolios' => $pfs]);
   }
 }
--- laravel/tpw/routes/web.php
+++ laravel/tpw/routes/web.php
@@ -12,7 +12,11 @@
 */
 
 Route::get('/', function () {
-    return redirect('/most_stars0001.html');
+    return redirect('/most_stars/1');
     // return view('welcome');
 });
-Route::get('/{path}','pathController@index');
+Route::get('/most_stars/{num}','pathController@most_stars');
+Route::get('/most_forks/{num}','pathController@most_forks');
+Route::get('/recently_update/{num}','pathController@recently_update');
+Route::get('/all_locations','pathController@all_locations');
+Route::get('/all_users','pathController@all_users');

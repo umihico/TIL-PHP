34f664d8b8965b1338f2439fff4842d3c64a6fa1
umihico <umihico@users.noreply.github.com>
2019-02-08 17:09:42 +0900

    add basic info

--- laravel/tpw/resources/views/menu.blade.php
+++ laravel/tpw/resources/views/menu.blade.php
@@ -1,22 +1,22 @@
 @section('menu')
 <div class="row">
   <div class="col-sm-6 col-md-4">
-    <h3><a href="most_stars/0">Most Stars</a></h3>
+    <h3><a href="/most_stars/0">Most Stars</a></h3>
   </div>
   <div class="col-sm-6 col-md-4">
-    <h3><a href="most_forks/0">Most Forks</a></h3>
+    <h3><a href="/most_forks/0">Most Forks</a></h3>
   </div>
   <div class="col-sm-6 col-md-4">
-    <h3><a href="recently_update/0">Recently Update</a></h3>
+    <h3><a href="/recently_update/0">Recently Update</a></h3>
   </div>
   <div class="col-sm-6 col-md-4">
-    <h3><a href="all_locations">All locations</a></h3>
+    <h3><a href="/all_locations">All locations</a></h3>
   </div>
   <div class="col-sm-6 col-md-4">
-    <h3><a href="all_users">All users</a></h3>
+    <h3><a href="/all_users">All users</a></h3>
   </div>
   <div class="col-sm-6 col-md-4">
-    <h3><a href="purpose">Purposes</a></h3>
+    <h3><a href="/purpose">Purposes</a></h3>
   </div>
 </div>
 @endsection
--- laravel/tpw/resources/views/showcase.blade.php
+++ laravel/tpw/resources/views/showcase.blade.php
@@ -5,6 +5,11 @@
      <div class="col-sm-6 col-md-4">
        <div class="userbox">
          <a href="https://umihico.github.io{{ $showcase->gif_path }}"><img src="https://umihico.github.io{{ $showcase->gif_path }}" alt="{{ $showcase->gif_path }}"></a>
+         <li>{{ $showcase->username}}</li>
+         <li><a href="https://github.com/{{ $showcase->full_name }}">repo: {{ $showcase->reponame }}</a></li>
+         <li><a href="{{ $showcase->html_url }}">portfolio website</a></li>
+         <li><a href="https://github.com/{{ $showcase->full_name }}/stargazers">{{ $showcase->stargazers_count }}stars</a></li>
+         <li><a href="https://github.com/{{ $showcase->full_name }}/members">{{ $showcase->forks }}forks</a></li>
        </div>
      </div>
      @endforeach

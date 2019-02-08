3e0fbb01dd4ead8aad9d9b95c43793d8a5af41db
umihico <umihico@users.noreply.github.com>
2019-02-08 16:27:29 +0900

    fix skill int to doble quoted string

--- laravel/tpw/json_to_sqlite.py
+++ laravel/tpw/json_to_sqlite.py
@@ -17,7 +17,9 @@ with closing(sqlite3.connect(dbname)) as conn:
     names=["geotags", "full_name", "forks", "gif_path", "gif_success", "homepage", "homepage_exist", "html_url", "pushed_at", "reponame", "skills", "stargazers_count", "username"]
     portfolio_list=[]
     for dic in json:
-        tuple_=[str(x) if isinstance(x,(list,dict,type(None))) else x for x in [dic[name] for name in names]]
+        skillset=dic['skills']
+        dic['skills']=[(lang,str(pie)) for lang,pie in skillset]
+        tuple_=[str(x).replace("'",'"') if isinstance(x,(list,dict,type(None))) else x for x in [dic[name] for name in names]]
         portfolio_list.append(tuple_)
     c.executemany(insert_sql, portfolio_list)
     conn.commit()

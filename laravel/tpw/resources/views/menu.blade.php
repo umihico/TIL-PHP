5b970c11c966456bdd40e4e6c645097e2fd1e9c9
umihico <umihico@users.noreply.github.com>
2019-02-08 15:51:18 +0900

    typo

--- laravel/tpw/resources/views/menu.blade.php
+++ laravel/tpw/resources/views/menu.blade.php
@@ -1,13 +1,13 @@
 @section('menu')
 <div class="row">
   <div class="col-sm-6 col-md-4">
-    <h3><a href="most_stars/0"></a>Most Stars</h3>
+    <h3><a href="most_stars/0">Most Stars</a></h3>
   </div>
   <div class="col-sm-6 col-md-4">
-    <h3><a href="most_forks/0"></a>Most Forks</h3>
+    <h3><a href="most_forks/0">Most Forks</a></h3>
   </div>
   <div class="col-sm-6 col-md-4">
-    <h3><a href="recently_update/0"></a>Recently Update</h3>
+    <h3><a href="recently_update/0">Recently Update</a></h3>
   </div>
   <div class="col-sm-6 col-md-4">
     <h3><a href="all_locations">All locations</a></h3>

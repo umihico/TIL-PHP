7b22a3716dd43795e251985f593a3fbc5485b5aa
umihico <umihico@users.noreply.github.com>
2019-02-12 14:01:29 +0900

    solve

--- pz/solver.php
+++ pz/solver.php
@@ -17,61 +17,34 @@ function stdin_to_intvals() {
 }
 function stdin_handler()
 {
-  list($first_x, $first_y)=stdin_to_intvals();
-  list($jump_power_front, $jump_power_right,$jump_power_back,$jump_power_left)=stdin_to_intvals();
   $n=stdin_to_intval();
+  $answer=trim(fgets(STDIN));
   for($i=0;$i<$n;++$i){
-    $orders[]=explode(' ',trim(fgets(STDIN)));
+    $keywords[]=trim(fgets(STDIN));
   }
-  return array($first_x, $first_y, $jump_power_front, $jump_power_right,$jump_power_back,$jump_power_left,$orders);
+  return array($answer,$keywords);
 }
 
-function multiply_directions($direction,$order_direction)
-{
-  $direction_to_clock = array(
-    "F" => 0,
-    "R" => 3,
-    "B" => 6,
-    "L" => 9,
-  );
-  $clock_direction=($direction_to_clock[$direction]+$direction_to_clock[$order_direction])% 12;
-  $moving_direction=array_flip($direction_to_clock)[$clock_direction];
-  return $moving_direction;
-}
-
-function move($x,$y,$moving_direction,$jump_power)
-{
-  $direction_base_funcs = array(
-    "F" => array(0,1),
-    "R" => array(1,0),
-    "B" => array(0,-1),
-    "L" => array(-1,0)
-  );
-  $x_jump_power=$direction_base_funcs[$moving_direction][0] * $jump_power;
-  $y_jump_power=$direction_base_funcs[$moving_direction][1] * $jump_power;
-  return array($x_jump_power+$x,$y_jump_power+$y);
-}
-function solve($first_x, $first_y, $jump_power_front, $jump_power_right,$jump_power_back,$jump_power_left,$orders)
-{
-  $x=$first_x;
-  $y=$first_y;
-  $direction="F";
-  $jump_powers = array(
-    "F" => $jump_power_front,
-    "R" => $jump_power_right,
-    "B" => $jump_power_back,
-    "L" => $jump_power_left
-  );
-  foreach ($orders as $order) {
-    if ($order[0]=="m") {
-      $moving_direction=multiply_directions($direction,$order[1]);
-      $jump_power=$jump_powers[$order[1]];
-      list($x,$y)=move($x,$y,$moving_direction,$jump_power);
-    } else {
-      $direction=multiply_directions($direction,$order[1]);
+function echo_result($answer,$keyword) {
+  if (preg_match("/".$answer."/", $keyword)){
+    echo "valid\n";
+    return;
+  }
+  for($i=0;$i<strlen($answer);++$i){
+    list($front,$back)=explode(",",substr_replace($answer, ",", $i, 0));
+    if (preg_match("/".$front."[a-z]{0,1}".$back."/", $keyword)){
+      echo "valid\n";
+      return;
     }
   }
-  echo $x." ".$y;
+  echo "invalid\n";
+}
+
+function solve($answer,$keywords)
+{
+  foreach ($keywords as $keyword) {
+    echo_result($answer,$keyword);
+  }
 }
 function main()
 {
--- pz/stdin.sh
+++ pz/stdin.sh
@@ -1,37 +1,26 @@
 php solver.php<< EOF
-2 2
-2 1 1 1
-8
-m F
-t R
-m F
-m F
-t B
-m L
-m L
-m B
+7
+paiza
+sdfpaizaoiu
+sdfpaizoiu
+paxiza
+paxizya
+ghepaizmakbn
+paizzza
+abcpadizzzaopq
 EOF
 php solver.php<< EOF
-621 -855
-2 6 3 6
-19
-t L
-t L
-t R
-m L
-m B
-t R
-t L
-t L
-t B
-t B
-m R
-t B
-m R
-t R
-t L
-t B
-m L
-m R
-t L
+11
+gbyw
+gbyw
+ggbyw
+gbyws
+b
+kwflzgltejlcaosivfsy
+gbyl
+ogbyw
+ejyt
+yjlih
+bzbrquhbujsxnnoxfsw
+ygowvgibywkw
 EOF

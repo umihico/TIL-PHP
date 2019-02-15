f395ea49fe767992c47103638b544c9858b29d20
umihico <umihico@users.noreply.github.com>
2019-02-15 15:34:05 +0900

    import by "use"

--- libs/guzzle/guzzle_basics.php
+++ libs/guzzle/guzzle_basics.php
@@ -1,11 +1,30 @@
 <?php
 require "vendor/autoload.php";
 
-$url = "http://google.com";
+function helloworld()
+{
+  $url = "http://google.com";
 
-$client = new \GuzzleHttp\Client();
-$res = $client->request("GET", $url);
+  $client = new \GuzzleHttp\Client();
+  $res = $client->request("GET", $url);
 
-echo $res->getBody()."\n";
-echo $res->getStatusCode()."\n";
-echo $res->getHeaderLine("content-type")."\n";
+  echo $res->getBody()."\n";
+  echo $res->getStatusCode()."\n";
+  echo $res->getHeaderLine("content-type")."\n";
+}
+
+use GuzzleHttp\Client;
+function helloworld2()
+{
+  $url = "http://google.com";
+
+  $client = new Client();
+  $res = $client->request("GET", $url);
+
+  echo $res->getBody()."\n";
+  echo $res->getStatusCode()."\n";
+  echo $res->getHeaderLine("content-type")."\n";
+}
+
+helloworld();
+helloworld2();
